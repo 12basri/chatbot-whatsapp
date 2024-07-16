8dea433baf53fd3dd530999e3de4a1ebe2637952
unknown <basri263@guru.smk.belajar.id>
2024-07-16 17:47:07 +0800

    first commit

---
+++ kirimwa.py
@@ -0,0 +1,11 @@
+import pywhatkit
+import pyautogui
+import time
+
+pywhatkit.sendwhatmsg("+6285397828095","Halo saya Basri",17,15)
+pywhatkit.sendwhatmsg("+6282349038663","Halo saya Basri",17,15)
+time.sleep(1)
+pyautogui.click(1)
+time.sleep(1)
+pyautogui.press(enter)
+
